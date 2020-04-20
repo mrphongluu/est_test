61ceda2902cf589a7abcbaef0abd5e91d95eb177
unknown <phong.lq@terra-drone.co.jp>
2020-04-20 20:47:30 +0700

    init

---
+++ config/database.php
@@ -0,0 +1,10 @@
+<?php
+
+define("DB_DRIVER", "mysql");
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "123123");
+define("DB_DATABASE", "est_test");
+define("DB_CHARSET", "utf8");
+
+?>
